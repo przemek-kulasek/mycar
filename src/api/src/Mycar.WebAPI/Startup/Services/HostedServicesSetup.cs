4a3dd52b19b09ca2f30b8d78cb2be18976311e24
przemyslaw.kulasek <przemyslaw.kulasek@bluesoft.com>
2023-09-12 09:59:14 +0200

    infrastructure, entity framework with postresql

--- src/api/src/Mycar.WebAPI/Startup/Services/HostedServicesSetup.cs
+++ src/api/src/Mycar.WebAPI/Startup/Services/HostedServicesSetup.cs
@@ -1,10 +1,12 @@
-﻿namespace Mycar.WebAPI.Startup.Services
+﻿using Mycar.WebAPI.Bootstrappers;
+
+namespace Mycar.WebAPI.Startup.Services
 {
     public static class HostedServicesSetup
     {
         public static IServiceCollection AddHostedServices(this IServiceCollection services)
         {
-            //services.AddHostedService<MycarBootstrapper>();
+            services.AddHostedService<MycarBootstrapper>();
             return services;
         }
     }
